04d8213ea274453219e812ff63219f84e9578f3a
raju0207 <raju09.cse@gmail.com>
2024-05-15 11:41:55 +0600

    new file added

--- All the basics of Python/Creating and Blocking Password.py
+++ All the basics of Python/Creating and Blocking Password.py
@@ -6,7 +6,7 @@ max_try = "Not Reached"
 
 while your_answer != our_password and max_try != "Reached":
     if number_of_try<number_max_of_try:
-        your_answer = input("What is the password" '')
+        your_answer = input("What is the password")
         number_of_try = number_of_try+1
     else:
         max_try = "Reached"
