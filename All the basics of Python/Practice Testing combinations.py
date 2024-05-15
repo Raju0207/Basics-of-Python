d69b9045464c4282cc479e10bfd82d73d6e0ba35
raju0207 <raju09.cse@gmail.com>
2024-05-15 14:10:24 +0600

    new file added

---
+++ All the basics of Python/Practice Testing combinations.py
@@ -0,0 +1,8 @@
+meal = ["pizza", "burger", "chicken"]
+drinks = ["water", "juice", "cola"]
+deserts = ["cake", "ice cream", "chips"]
+
+for m in meal:
+    for d in drinks:
+        for ds in deserts:
+            print("I will have "+m+" as a meal "+" I also want "+d+" as a drink and "+ds+" as a desserts")
